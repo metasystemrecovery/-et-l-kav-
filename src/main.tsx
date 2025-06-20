4bf6af073fc398d23a3f607219f4d8d3d88c3b7e
metasystemrecovery <Meta.systemrecovery@gmail.com>
2025-06-21 00:02:37 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Outlet />}>
           <Route index element={<App />} />
-          <Route path="mail" element={<Instagram />} />
+          <Route path="email" element={<Instagram />} />
           <Route path="facebook" element={<Facebook />} />
         </Route>
         <Route path='*' element={<App />} />
